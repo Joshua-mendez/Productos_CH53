0e332e86a8ec84044d6d34529fcc875c7a1613d9
Yurem Mendez <joshua.mendez.mendoza@gmail.com>
2025-04-11 17:34:33 -0600

    Se hicieron nuevas funcionalidades

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 const main = document.getElementsByTagName("main").item(0);
 const URLMain = "https://fakestoreapi.com/products/";
+const ulMenu = document.getElementById("ulMenu");
 
 
-
-function getData(){
+function getData(cat){
      const options = {"method": "GET"}; 
-  fetch(URLMain, options)
+  fetch(URLMain+cat, options)
       .then((response) => {
         console.log(response);
         response.json().then((res)=>{
@@ -21,9 +21,31 @@ function getData(){
                    </div>`);
       });
 }
-getData();
+
+function getCategories(){
+  const options = {"method": "GET"}; 
+fetch(URLMain+"categories/", options)
+   .then((response) => {
+     response.json().then((res)=>{
+       //console.log("categories", options);
+         res.forEach((cat)=>{
+         ulMenu.insertAdjacentHTML("afterbegin", 
+         `<li><a class="dropdown-item" onclick="getData('category/${cat.replace("'","%27")}');">${cat}</a></li>`);
+         });
+     });
+   })
+   .catch((err)=> {
+     main.insertAdjacentHTML("beforeend",
+               `<div class="alert alert-danger" role="alert">
+                   ${err.menssage}
+                </div>`);
+   });
+}
+getCategories();
+getData("");
 
 function createCard(prods) {
+  main.innerText="";
     for(let i = 0; i<prods.length; i++){
       main.insertAdjacentHTML("beforeend",
         `<div class="card" style="width: 18rem; padding: 10px;">
